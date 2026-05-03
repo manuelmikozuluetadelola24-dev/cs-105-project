73fae00c1d6d46fab2384496eddbca96e3160b67
manuelmikozuluetadelola24-dev <manuelmikozulueta.delola24@bicol-u.edu.ph>
2026-05-03 21:11:22 +0800

    fixed some formatting

--- app/db.py
+++ app/db.py
@@ -17,7 +17,8 @@ def initializeConnection(*config):
 def listDeliveries(connection):
 	with connection:
 		with connection.cursor() as cursor:
-			sql = "SELECT `DELIVERY`.`delivery_id`, `customer_name`, `ORDERS`.`order_id`, `delivery_date`, `DELIVERY`.`status`, `delivered_by`, CONCAT(`delivery_street`, `delivery_barangay`, `delivery_city`) AS `delivery_address` FROM `DELIVERY` JOIN `ORDERS` ON `DELIVERY`.`order_id` = `ORDERS`.`order_id` JOIN `CUSTOMER` ON `CUSTOMER`.`customer_id` = `CUSTOMER`.`customer_id` ORDER BY `delivery_date` DESC"
+			sql = "SELECT `DELIVERY`.`delivery_id`, `customer_name`, `ORDERS`.`order_id`, `delivery_date`, `DELIVERY`.`status`, `delivered_by`, CONCAT(`delivery_street`, ' ', `delivery_barangay`, ' ', `delivery_city`) AS `delivery_address` FROM `DELIVERY` JOIN `ORDERS` ON `DELIVERY`.`order_id` = `ORDERS`.`order_id` JOIN `CUSTOMER` ON `CUSTOMER`.`customer_id` = `CUSTOMER`.`customer_id` ORDER BY `delivery_date` DESC"
 			cursor.execute(sql)
-			result = cursor.fetchone()
-			print(result)
+			result = cursor.fetchall()
+			for row in result:
+				print(row)
