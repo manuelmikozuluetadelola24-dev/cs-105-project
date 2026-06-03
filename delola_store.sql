3d10815fe4affac9150a30329dcd0c8141c5292a
mel-belgado <m.jullian.belgado@gmail.com>
2026-06-03 12:04:43 +0800

    Add role column

--- delola_store.sql
+++ delola_store.sql
@@ -103,6 +103,7 @@ CREATE TABLE IF NOT EXISTS delola_store.`users` (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     UNIQUE (username)
